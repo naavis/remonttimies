0a28a35a8b071e169421ba22b14a61c748e7a8ef
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2014-10-26 01:24:22 +0300

    Add placeholder shading for raytracing.

--- Remonttimies/Raytracing/Renderer.cpp
+++ Remonttimies/Raytracing/Renderer.cpp
@@ -21,9 +21,11 @@ Image Renderer::Render(unsigned int width, unsigned int height)
 			float localY = 2.0f * static_cast<float>(y) / height - 1.0f;
 			Ray ray = camera->GenerateRay(localX, -localY);
 			RaycastResult result = bvhTree->Intersect(ray);
+			glm::vec3 sunLight = glm::normalize(glm::vec3(0.2f, -0.3f, 0.5f));
 			if (result.hit) {
 				glm::vec3 materialColor = scene->GetMaterial(result.materialIndex).GetDiffuseColor();
-				image.SetPixel(x, y, materialColor);
+				auto shade = glm::max(0.0f, glm::dot(result.normal, -sunLight));
+				image.SetPixel(x, y, shade * materialColor);
 			}
 			else {
 				image.SetPixel(x, y, glm::vec3(0.0f));
